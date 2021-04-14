c2edf86cd8c6aeb284192db38ce66a46f32cc9f8
Daniela <elle.hiems@gmail.com>
2021-04-14 20:15:10 +0300

    refactored workers (refactor ss, add er)

---
+++ src/components/lab1_worker_ss.erl
@@ -0,0 +1,49 @@
+-module(lab1_worker_ss).
+-behaviour(gen_server).
+
+-export([start_link/0, init/1, work/2, handle_call/3, handle_cast/2]).
+
+start_link() -> 
+    gen_server:start_link(?MODULE, [], []).
+
+init([]) -> 
+    io:format("~p: ~p~n", ["Worker SS", self()]),
+    {ok, {}}.
+
+work(Pid, IdedTweet) ->
+    gen_server:cast(Pid, {work, IdedTweet}).
+
+
+handle_call(_Request, _From, _State) ->
+    {noreply, _State}.
+
+handle_cast({work, IdedTweet}, State) ->
+    % sleep(),
+    {Id, Tweet} = IdedTweet,
+    #{
+        <<"text">> := Text
+    } = Tweet,
+    TweetSS = compute(Text),
+    lab2_aggregator:aggregate({Id, TweetSS}),
+    % io:format("~p: id ~p tweetSS ~p~n", [self(), Id, TweetSS]),
+    {noreply, State}.
+
+compute(Text) ->
+    TextList = unicode:characters_to_list(Text, utf8),
+    LowText = string:lowercase(TextList),
+    Tokens = string:tokens(LowText, "\n ,.?!;:/'"),
+    Sum = lists:sum(lists:map(
+        fun (Word) -> lab1_emotional_score:get_score(Word) end,
+		Tokens
+    )),
+    Sum / length(Tokens).
+
+sleep() ->
+    MSec = round(450 * rand:uniform() + 50),
+    wait(MSec).
+
+wait(MSec) -> 
+    receive  
+    after 
+        MSec -> ok 
+    end.
---
+++ src/components/lab2_worker_er.erl
@@ -0,0 +1,48 @@
+-module(lab2_worker_er).
+-behaviour(gen_server).
+
+-export([start_link/0, init/1, work/2, handle_call/3, handle_cast/2, compute/3]).
+
+start_link() -> 
+    gen_server:start_link(?MODULE, [], []).
+
+init([]) -> 
+    io:format("~p: ~p~n", ["Worker ER", self()]),
+    {ok, {}}.
+
+work(Pid, IdedTweet) ->
+    gen_server:cast(Pid, {work, IdedTweet}).
+
+
+handle_call(_Request, _From, _State) ->
+    {noreply, _State}.
+
+handle_cast({work, IdedTweet}, State) ->
+    % sleep(),
+    {Id, Tweet} = IdedTweet,
+    #{
+        <<"retweet_count">> := Retweets,
+        <<"favorite_count">> := Favourites,
+        <<"user">> := #{
+            <<"followers_count">> := Followers
+        }
+    } = Tweet,
+    TweetER = compute(Favourites, Retweets, Followers),
+    lab2_aggregator:aggregate({Id, TweetER}),
+    % io:format("~p: id ~p tweetER ~p~n", [self(), Id, TweetER]),
+    {noreply, State}.
+
+compute(Favourites, Retweets, Followers) when Followers =/= 0 ->
+    (Favourites + Retweets) / Followers;
+compute(_, _, Followers) when Followers =:= 0 ->    
+    0.
+
+sleep() ->
+    MSec = round(450 * rand:uniform() + 50),
+    wait(MSec).
+
+wait(MSec) -> 
+    receive  
+    after 
+        MSec -> ok 
+    end.
