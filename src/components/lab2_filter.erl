182a0f2682dcbde4e20ac4e6fdc696751325d25d
Daniela <elle.hiems@gmail.com>
2021-04-14 20:13:56 +0300

    modified filter (now extracts retweets)

--- src/components/lab2_filter.erl
+++ src/components/lab2_filter.erl
@@ -20,28 +20,50 @@ handle_cast({event, Event}, State) ->
     IsJson = jsx:is_json(Data),
 
     process(Data, State, IsJson),
-    {noreply, State + 1}.
+    {noreply, State + 2}.
 
 process(Data, State, IsJson) when IsJson == true ->
     Json = jsx:decode(Data),
-    #{<<"message">> := #{<<"tweet">> := Tweet}} = Json,
-    IdedTweet = add_id(State, Tweet),
+
+    #{
+        <<"message">> := #{
+            <<"tweet">> := Tweet
+        }
+    } = Json,
+    Retweet = maps:get(<<"retweeted_status">>, Tweet, #{}),
+    % io:format("retweet: ~p~n", [Retweet]),    
+
+    IdedTweets = add_ids(State, [Tweet, Retweet]),
+    % io:format("Idedtweets: ~p~n", [IdedTweets]),    
 
     PoolSupER = lab1_supersup:get_child(lab1_pool_sup_er),
     PoolSupSS = lab1_supersup:get_child(lab1_pool_sup_ss),
     RouterER = lab1_pool_sup:get_child(PoolSupER, lab1_router),
     RouterSS = lab1_pool_sup:get_child(PoolSupSS, lab1_router),
 
-    lab1_router:route(RouterER, IdedTweet),
-    lab1_router:route(RouterSS, IdedTweet),
-    % lab2_aggregator:aggregate(IdedTweet),
-    % io:format("~p: ~p~n", [self(), IdedTweet]),
-    ok;
+    send(IdedTweets, RouterSS, RouterER);
+
 process(_, _, IsJson) when IsJson == false ->
     ok.
 
-add_id(State, Tweet) ->
-    {State, Tweet}.
+
+add_ids(Id, Tweets) -> 
+    add_ids(Id, Tweets, []).
+ 
+add_ids(_, [], IdedTweets) ->
+    IdedTweets;
+add_ids(Id, [Tweet|T], IdedTweets) when Tweet =/= #{} -> 
+    IdedTweet = {Id, Tweet},
+    add_ids(Id + 1, T, [IdedTweet|IdedTweets]);
+add_ids(Id, [Tweet|T], IdedTweets) when Tweet =:= #{} -> 
+    add_ids(Id, T, IdedTweets).
 
 
-
+send([], _, _) ->
+    ok;
+send([IdedTweet|T], RouterSS, RouterER) ->
+    lab1_router:route(RouterER, IdedTweet),
+    lab1_router:route(RouterSS, IdedTweet),
+    lab2_aggregator:aggregate(IdedTweet),
+    % io:format("~p: ~p~n", [self(), IdedTweet]),
+    send(T, RouterSS, RouterER).
