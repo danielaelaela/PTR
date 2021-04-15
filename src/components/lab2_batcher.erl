ca82cfee09202690cc32efa04cba0e122a0b1b37
Daniela <elle.hiems@gmail.com>
2021-04-15 20:06:02 +0300

    add storage actor (initial commit, modified batcher)

--- src/components/lab2_batcher.erl
+++ src/components/lab2_batcher.erl
@@ -1,31 +1,39 @@
 -module(lab2_batcher).
 -behaviour(gen_server).
 
--export([start_link/0, init/1, write_sscore/1, handle_cast/2, handle_info/2]).
+-export([start_link/0, init/1, batch/1, handle_call/3, handle_cast/2, handle_info/2]).
+
+-define(BATCHSIZE, 128).
+-define(TIMELIMIT, 200).
+
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
     io:format("~p: ~p~n", ["Batcher", self()]),
-    TimerRef = erlang:start_timer(5000, self(), timeout),
+    TimerRef = erlang:start_timer(?TIMELIMIT, self(), timeout),
     {ok, {[], TimerRef}}.
 
-write_sscore(SScore) ->
-    gen_server:cast(?MODULE, {sscore, SScore}).
+batch(Map) ->
+    gen_server:cast(?MODULE, {map, Map}).
 
-handle_cast({sscore, SScore}, State) ->
+
+handle_call(_, _, State) ->
+    {noreply, State}.
+
+handle_cast({map, Map}, State) ->
     {Batch, TimerRef} = State,
-    NewBatch = [SScore | Batch],
+    NewBatch = [Map | Batch],
     erlang:start_timer(0, self(), limit),
     {noreply, {NewBatch, TimerRef}}.
 
 handle_info({_, _, limit}, State) ->
     {Batch, TimerRef} = State,
     case length(Batch) of
-        3 -> 
-            % lab2_storage:write_batch(State),
-            io:format("Batch sent from limit: ~p~n", [Batch]),
+        ?BATCHSIZE -> 
+            lab2_storage:write_batch(Batch),
+            io:format("Batch sent from limit: ~p~n", [length(Batch)]),
             erlang:cancel_timer(TimerRef, []), 
             NewTimerRef = erlang:start_timer(5000, self(), timeout),  
             {noreply, {[], NewTimerRef}};
@@ -33,10 +41,10 @@ handle_info({_, _, limit}, State) ->
             {noreply, State}
     end;
 handle_info({_, _, timeout}, State) ->
-    {Batch, TimerRef} = State,
-%    lab2_storage:write_batch(State),
-    io:format("Batch sent: ~p~n", [Batch]),
+    {Batch, _} = State,
+    lab2_storage:write_batch(Batch),
+    io:format("Batch sent from timeout: ~p~n", [length(Batch)]),
 
-    NewTimerRef = erlang:start_timer(5000, self(), timeout),
+    NewTimerRef = erlang:start_timer(?TIMELIMIT, self(), timeout),
 
     {noreply, {[], NewTimerRef}}.
---
+++ src/components/lab2_storage.erl
@@ -0,0 +1,88 @@
+-module(lab2_storage).
+-behaviour(gen_server).
+
+-export([start_link/0, init/1, write_batch/1, handle_call/3, handle_cast/2, handle_info/2]).
+
+-define(EVENTTABLE, event).
+-define(TWEETTABLE, tweet).
+-define(USERTABLE, user).
+
+-define(LOGTIME, 1000).
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+init([]) ->
+    io:format("~p: ~p~n", ["Storage", self()]),
+
+    ets:new(?EVENTTABLE, [set, named_table]),
+    ets:new(?TWEETTABLE, [set, named_table]),
+    ets:new(?USERTABLE, [set, named_table]),
+    
+    erlang:start_timer(5000, self(), timeout),
+    {ok, {}}.
+
+write_batch(Batch) ->
+    gen_server:cast(?MODULE, {batch, Batch}).
+
+
+handle_call(_, _, State) ->
+    {noreply, State}.
+
+handle_cast({batch, Batch}, _State) ->
+    {EventBatch, TweetBatch, UserBatch} = separate_users(Batch), 
+
+    ets:insert(?EVENTTABLE, EventBatch),
+    ets:insert(?TWEETTABLE, TweetBatch),
+    ets:insert(?USERTABLE, UserBatch),
+
+    {noreply, _State}.
+
+handle_info({timeout, _, _}, _State) ->
+    EventInfo = ets:info(?EVENTTABLE),
+    TweetInfo = ets:info(?TWEETTABLE),
+    UserInfo = ets:info(?USERTABLE),
+
+    EventSize = lists:keyfind(size, 1, EventInfo),
+    TweetSize = lists:keyfind(size, 1, TweetInfo),
+    UserSize = lists:keyfind(size, 1, UserInfo),
+
+    io:format("Event Storage: ~p~n", [EventSize]),
+    io:format("Tweet Storage: ~p~n", [TweetSize]),
+    io:format("User Storage: ~p~n", [UserSize]),
+
+    % io:format("User Storage: ~p~n", [ets:tab2list(?TWEETTABLE)]),
+    
+    erlang:start_timer(?LOGTIME, self(), timeout),
+    {noreply, _State}.
+
+
+separate_users(Batch) -> 
+    separate_users(Batch, {[], [], []}).
+ 
+separate_users([], {EventBatch, TweetBatch, UserBatch}) ->
+    {EventBatch, TweetBatch, UserBatch};
+separate_users([H|T], {EventBatch, TweetBatch, UserBatch}) ->
+    {Event, Tweet, User} = split(H),
+    separate_users(T, {[Event|EventBatch], [Tweet|TweetBatch], [User|UserBatch]}).
+
+split(Map) ->
+    #{
+        id := Id,
+        tweet := Twt,
+        ss := Ss,
+        er := Er
+    } = Map,
+
+    #{
+        <<"user">> := UserMap
+    } = Twt,
+    TweetMap = maps:without([<<"user">>], Twt),
+
+    TweetId = crypto:hash(md5, jsx:encode(TweetMap)),
+    UserId = crypto:hash(md5, jsx:encode(UserMap)),
+
+    Event = {Id, TweetId, UserId, Ss, Er},
+    Tweet = {TweetId, TweetMap},
+    User = {UserId, UserMap},
+    {Event, Tweet, User}.
